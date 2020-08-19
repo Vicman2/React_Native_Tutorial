8b255a8e9d018f6427dca490a613224e56945461
Vicman2 <vicmanthebest@gmail.com>
2020-08-19 07:04:08 +0000

    Added Displaying outputs

--- App.js
+++ App.js
@@ -1,21 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Vicman storming mobile with react-native</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+
+class App extends Component{
+  state ={
+    placeName:"", 
+    places : []
+  }
+  placeNameChangeHandler = (val) => {
+    this.setState({
+      placeName: val
+    })
+  }
+  placeSubmiteHandler= () => {
+    if(this.state.placeName.trim() === ""){
+      return;
+    }
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(prevState.placeName)
+      }
+    })
+  }
+  render(){
+    const placesOutput = this.state.places.map((place, index) => {
+      return(
+        <Text key={index}> {place} </Text>
+      )
+    })
+    return (
+      <View style={styles.container}>
+        <View style={styles.InputContainer}>
+          <TextInput
+          placeholder="An Awesome Vicman"
+          style={styles.placeInput}
+          value={this.state.placeName}
+          onChangeText={this.placeNameChangeHandler}/>
+          <Button 
+          style={styles.placeButton}
+          title="Add" 
+          onPress={this.placeSubmiteHandler}
+          />
+        </View>
+        <View>
+          {placesOutput}
+        </View>
+      </View>
+    );
+  }
 }
+export default App
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding:26,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  InputContainer: {
+    // flex:1,
+    width:'100%',
+    flexDirection:'row', 
+    justifyContent:'space-around',
+    alignItems:'center'
+  }, 
+  placeInput:{
+    width: "70%"
+  }, 
+  placeButton: {
+    width:"30%"
+  }
 });
